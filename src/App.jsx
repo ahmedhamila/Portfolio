2d8f27d34325f5bd1d3de09c4139024ab9e624ac
ahmedhamila <csahmed17@gmail.com>
2023-10-02 01:16:27 +0100

    Particles

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,13 @@ import {
   Works,
   StarsCanvas,
 } from "./components";
+import Particlesbackground from "./components/Particlesbackground";
 
 const App = () => {
   return (
     <BrowserRouter>
       <div className="relative z-0 bg-primary">
+        <Particlesbackground />
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
           <Navbar />
           <Hero />
--- tailwind.config.js
+++ tailwind.config.js
@@ -18,9 +18,6 @@ module.exports = withMT({
       screens: {
         xs: "450px",
       },
-      backgroundImage: {
-        "hero-pattern": "url('/src/assets/herobg.png')",
-      },
     },
   },
   plugins: [],
